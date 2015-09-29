4b8cdd06be1c3d181ecd5d73107f93edddd6a459
emz5051 <emz5051@psu.edu>
2015-09-28 21:16:45 -0400

    modified webservice with base64 encoding

--- jwtClient.py
+++ jwtClient.py
@@ -1,8 +1,17 @@
 from soaplib.client import make_service_client
 from jwtlogin import HelloService
+import jwt
+import base64
 
 client = make_service_client('http://localhost:8080/jwt', HelloService())
 usr = raw_input("Enter your username: ")
 pswd = raw_input("Enter your password: ")
-response = client.jwt(usr, pswd)
+
+key = 'secret'
+payload = {'usr':usr,'pswd':pswd}
+token = jwt.encode(payload, key, 'HS256')
+
+token = base64.urlsafe_b64encode(token)
+
+response = client.jwt(token)
 print response
