b2b318cb0e532738cb2ced697d34eba843951a2c
Ethan Zavaglia <emz5051@Team3.(none)>
2015-10-15 07:28:06 -0400

    added unit testing

--- auth.py
+++ auth.py
@@ -5,16 +5,28 @@ import log
 
 FNULL = open(os.devnull, 'w')
 
-try:
-    output = subprocess.check_call("klist", shell=False, stdout=FNULL, stderr=subprocess.STDOUT,)
-except subprocess.CalledProcessError as e:
-    output = 1
+def kcheck():
+    try:
+        output = subprocess.check_call("klist", shell=False, stdout=FNULL, stderr=subprocess.STDOUT,)
+    except subprocess.CalledProcessError as e:
+        output = 1
 
-if output == 0:
+    return output
+
+usr = raw_input("Enter your username: ")
+
+o = subprocess.call(["kinit",  usr])
+
+if o == 1:
+    subprocess.call("kdestroy", stdout=FNULL)
+
+cred = kcheck()
+
+if cred == 0:
     print "You are authorized"
-    token = jwtHandler.jwtEncode("test", "test1")
+    token = jwtHandler.jwtEncode(usr, "test1")
     print token
-    log.login('emz5051', 'Login Successful')
-if output == 1:
+    log.login(usr, 'Login Successful')
+if cred == 1:
     print "No Ticket Found. Please try again."
-    log.login('emz5051', 'Login Failed')
+    log.login(usr, 'Login Failed')
